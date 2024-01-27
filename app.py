401d11ce8b20475271004bea959c2ac3ad57a29f
pfbrodon <pfbrodon@gmail.com>
2024-01-27 18:22:13 -0300

    galeria

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -18,6 +19,18 @@ def destacados():
 @app.route("/contacto")
 def contacto():
     return render_template('contacto.html')
+
+@app.route('/galeria_1')
+def galeria_1():
+    # Ruta de la carpeta de imágenes
+    ruta_imagenes = os.path.join(app.static_folder, 'images')
+
+    # Obtener la lista de nombres de archivo de imágenes
+    imagenes = [imagen for imagen in os.listdir(ruta_imagenes) if imagen.endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+    print(imagenes)
+    # Renderizar la plantilla con la lista de imágenes
+    return render_template('galeria_1.html', imagenes=imagenes)
+
 
 if __name__=='__main__':  
     app.run(debug=True, port=5000)    # ejecuta el servidor Flask en el puerto 5000
